243c1030d8bd71c232688bea3170b25f0b342b52
Vanessa Chambers <vanessachambers6@gmail.com>
2020-11-22 00:35:08 -0600

    changed location in filetree

---
+++ src/components/about/landingPageTags.js
@@ -0,0 +1,133 @@
+/* COLOR CODING 
+courses + academics - #4aa564
+student orgs - #00a6d2
+locations - #ff6b84
+identity/experience - #f9c642
+*/
+
+const academics = '#d0cde1';
+const orgs = '#36354c';
+const locations = '#f2f2f2';
+const idExp = '#ff8f8f';
+
+const landingPageTags = [
+    // Anri
+    {
+        title: "MATH101",
+        color: academics,
+    }, 
+    {
+        title: "Study Abroad",
+        color: idExp,
+    }, 
+    {
+        title: "Widener Library",
+        color: locations,
+    }, 
+    {
+        title: "Vegan",
+        color: idExp,
+    }, 
+    {
+        title: "Students with Disability",
+        color: idExp,
+    }, 
+    {
+        title: "PST/PDT",
+        color: locations,
+    }, 
+    {
+        title: "Feminism",
+        color: idExp,
+    }, 
+    {
+        title: "Career Networking",
+        color: academics,
+    }, 
+    {
+        title: "DACA",
+        color: idExp,
+    }, 
+    {
+        title: "Club Basketball",
+        color: orgs,
+    }, 
+    {
+        title: "Intro Swahili",
+        color: academics,
+    }, 
+    {
+        title: "Mardi Gras", 
+        color: idExp,
+    },
+    {
+        title: "Financial Literacy", 
+        color: idExp,
+    },
+    {
+        title: "Indigenous Peoples", 
+        color: idExp,
+    },
+    {
+        title: "Astrophysics", 
+        color: academics,
+    },
+    {
+        title: "LGBTQ+",
+        color: idExp,
+    }, 
+    {
+        title: "Women in STEM",
+        color: orgs,
+    }, 
+    {
+        title: "Intersectionality",
+        color: idExp,
+    }, 
+    {
+        title: "LSAT",
+        color: academics,
+    }, 
+    {
+        title: "Running and Triathlon Club",
+        color: orgs,
+    }, 
+    {
+        title: "California",
+        color: locations,
+    }, 
+    {
+        title: "Buddhist",
+        color: idExp,
+    }, 
+    {
+        title: "Venture Capital",
+        color: idExp,
+    }, 
+    {
+        title: "Chuseok",
+        color: idExp,
+    }, 
+    {
+        title: "Art History",
+        color: academics,
+    }, 
+    {
+        title: "Black Student Union",
+        color: orgs,
+    }, 
+    {
+        title: "International Students",
+        color: idExp,
+    }, 
+    {
+        title: "Hackathons",
+        color: idExp,
+    }, 
+    {
+        title: "Intramural Sports",
+        color: orgs,
+    }, 
+];
+
+export default landingPageTags;
---
+++ src/components/team/teamMemberTags.js
@@ -0,0 +1,112 @@
+/* COLOR CODING 
+courses + academics - #d0cde1
+student orgs - #36354c
+locations - #f2f2f2
+identity/experience - #ff8f8f
+*/
+
+const academics = '#d0cde1';
+const orgs = '#36354c';
+const locations = '#f2f2f2';
+const idExp = '#ff8f8f';
+
+const TeamMemberTags = [
+    [
+        // Anri
+        {
+            title: "Toronto Blue Jays", 
+            color: academics,
+        },
+        {
+            title: "Astronomy", 
+            color: orgs,
+        },
+        {
+            title: "Ketchup Chips",
+            color: locations,
+        }, 
+        {
+            title: "Bagged Milk", 
+            color: idExp,
+        },
+    ],
+
+    [
+        // Jenny 
+        {
+            title: "Broadway Jazz",
+            color: locations,
+        }, 
+        {
+            title: "Science Fiction", 
+            color: academics,
+        },
+        {
+            title: "Sketching", 
+            color: idExp, 
+        },
+        {
+            title: "Racquet Sports", 
+            color: orgs,
+        },
+    ],
+
+    [
+        {
+            title: "Korean", 
+            color: idExp,
+        },
+        {
+            title: "Musicals", 
+            color: orgs,
+        },
+        {
+            title: "Women in Tech", 
+            color: locations,
+        },
+        {
+            title: "Sports", 
+            color: academics,
+        },  
+    ],
+
+    [
+        {
+            title: "Student Athlete",
+            color: academics,
+        }, 
+        {
+            title: "Vegan", 
+            color: locations,
+        },
+        {
+            title: "CS", 
+            color: orgs,
+        },
+        {
+            title: "Social Justice", 
+            color: idExp,
+        },
+    ],
+
+    [
+        {
+            title: "Longboarding", 
+            color: idExp,
+        },
+        {
+            title: "BIPOC", 
+            color: academics,
+        },
+        {
+            title: "Animation", 
+            color: orgs,
+        },
+        {
+            title: "MIT Gymnastics", 
+            color: locations,
+        },
+    ],
+];
+
+export default TeamMemberTags;
