fdf8b5fb9d183244d01d2781b1651f05e52ab382
Vanessa Chambers <vanessachambers6@gmail.com>
2020-11-22 00:30:15 -0600

    Removed unused files

--- src/components/ThemedLink.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-const ThemedLink = (props) => {
-  return <Link to={props.to} style={{color: '#3f3f5b'}}>{props.children}</Link>
-};
-
-export default ThemedLink;
